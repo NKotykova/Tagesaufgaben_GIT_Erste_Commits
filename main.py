29c3c72a677aebf00fb983a9f2ca0e7376f58ce5
NKotykova <nkotykova@gmail.com>
2025-06-16 17:08:11 +0200

    Class Circle erstellt wird

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
-print("Moin!")
+class Circle:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
 
-def meine_funktion():
-    print("Hallo")
+    def __str__(self):
+        return f"Circle(x={self.x}, y={self.y}, radius={self.radius})"
+
+
+
+        
